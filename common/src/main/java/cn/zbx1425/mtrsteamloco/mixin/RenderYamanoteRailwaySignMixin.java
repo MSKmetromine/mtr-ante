b85d2c19960212e6f103e9ebce128aa1092bfa05
A. Polyakhov <a.polyakhov@mail.ru>
2026-05-24 10:46:08 -0400

    Fixed station decoration sign rendering.

--- common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/RenderYamanoteRailwaySignMixin.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/RenderYamanoteRailwaySignMixin.java
@@ -2,17 +2,23 @@ package cn.zbx1425.mtrsteamloco.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import mtr.block.BlockRailwaySign;
-import mtr.render.RenderRailwaySign;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.Direction;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(RenderRailwaySign.class)
-public class RenderRailwaySignMixin {
+#if MC_VERSION < "12000"
+import top.mcmtr.blocks.BlockYamanoteRailwaySign;
+#endif
+
+@Pseudo
+@Mixin(targets = "top/mcmtr/render/RenderYamanoteRailwaySign")
+public class RenderYamanoteRailwaySignMixin {
+    #if MC_VERSION < "12000"
     @Redirect(method = "lambda$render$0", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V", ordinal = 0))
-    private static void lambdaRender$0(PoseStack instance, double x, double y, double z, BlockRailwaySign.TileEntityRailwaySign entity) {
+    private static void lambdaRender$0(PoseStack instance, double x, double y, double z, BlockYamanoteRailwaySign.TileEntityRailwaySign entity) {
         var cameraPos = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
 
         instance.translate(
@@ -21,4 +27,5 @@ public class RenderRailwaySignMixin {
                 0.5 + (double) entity.getBlockPos().getZ() - cameraPos.z()
         );
     }
+    #endif
 }
